ad5d13e6709c3487a0f103928df9c9e3e0a5a04b
Chris Poore <poorec@ainfosec.com>
2022-12-21 20:46:47 -0500

    Fixing IQ Playback tab

--- dashboard.py
+++ dashboard.py
@@ -12817,7 +12817,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             # IQ Record
             spinbox_frequency = QtWidgets.QDoubleSpinBox(self)
             spinbox_frequency.setMaximum(6000)
-            spinbox_frequency.setMinimum(50)
+            spinbox_frequency.setMinimum(70)
             spinbox_frequency.setAlignment(QtCore.Qt.AlignCenter)
             self.tableWidget_iq_record.setCellWidget(0,1,spinbox_frequency)
             comboBox_channel = QtWidgets.QComboBox(self)
@@ -12843,7 +12843,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             # IQ Playback
             playback_spinbox_frequency = QtWidgets.QDoubleSpinBox(self)
             playback_spinbox_frequency.setMaximum(6000)
-            playback_spinbox_frequency.setMinimum(50)
+            playback_spinbox_frequency.setMinimum(70)
             playback_spinbox_frequency.setAlignment(QtCore.Qt.AlignCenter)
             self.tableWidget_iq_playback.setCellWidget(0,0,playback_spinbox_frequency)
             comboBox_playback_channel = QtWidgets.QComboBox(self)            
@@ -12872,7 +12872,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             # IQ Record
             spinbox_frequency = QtWidgets.QDoubleSpinBox(self)
             spinbox_frequency.setMaximum(6000)
-            spinbox_frequency.setMinimum(50)
+            spinbox_frequency.setMinimum(1)
             spinbox_frequency.setAlignment(QtCore.Qt.AlignCenter)
             self.tableWidget_iq_record.setCellWidget(0,1,spinbox_frequency)
             comboBox_channel = QtWidgets.QComboBox(self)
@@ -12896,7 +12896,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             # IQ Playback
             playback_spinbox_frequency = QtWidgets.QDoubleSpinBox(self)
             playback_spinbox_frequency.setMaximum(6000)
-            playback_spinbox_frequency.setMinimum(50)
+            playback_spinbox_frequency.setMinimum(1)
             playback_spinbox_frequency.setAlignment(QtCore.Qt.AlignCenter)
             self.tableWidget_iq_playback.setCellWidget(0,0,playback_spinbox_frequency)
             comboBox_playback_channel = QtWidgets.QComboBox(self)            
@@ -12977,7 +12977,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             # IQ Record
             spinbox_frequency = QtWidgets.QDoubleSpinBox(self)
             spinbox_frequency.setMaximum(6000)
-            spinbox_frequency.setMinimum(50)
+            spinbox_frequency.setMinimum(70)
             spinbox_frequency.setAlignment(QtCore.Qt.AlignCenter)
             self.tableWidget_iq_record.setCellWidget(0,1,spinbox_frequency)
             comboBox_channel = QtWidgets.QComboBox(self)
@@ -13003,7 +13003,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             # IQ Playback
             playback_spinbox_frequency = QtWidgets.QDoubleSpinBox(self)
             playback_spinbox_frequency.setMaximum(6000)
-            playback_spinbox_frequency.setMinimum(50)
+            playback_spinbox_frequency.setMinimum(70)
             playback_spinbox_frequency.setAlignment(QtCore.Qt.AlignCenter)
             self.tableWidget_iq_playback.setCellWidget(0,0,playback_spinbox_frequency)
             comboBox_playback_channel = QtWidgets.QComboBox(self)            
@@ -15718,11 +15718,20 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         if self.pushButton_iq_playback.text() == "Play":
             try:
                 # Get the Values from the Table
-                get_frequency = str(self.tableWidget_iq_playback.item(0,0).text())
+                try:
+                    get_frequency = str(self.tableWidget_iq_playback.cellWidget(0,0).value())
+                except:
+                    get_frequency = str(self.tableWidget_iq_playback.item(0,0).text())
                 get_channel = str(self.tableWidget_iq_playback.cellWidget(0,1).currentText())
                 get_antenna = str(self.tableWidget_iq_playback.cellWidget(0,2).currentText())
-                get_gain = str(self.tableWidget_iq_playback.item(0,3).text())
-                get_sample_rate = str(self.tableWidget_iq_playback.item(0,4).text()) 
+                try:
+                    get_gain = str(self.tableWidget_iq_playback.cellWidget(0,3).value())
+                except:
+                    get_gain = str(self.tableWidget_iq_playback.item(0,3).text())
+                try:
+                    get_sample_rate = str(self.tableWidget_iq_playback.cellWidget(0,4).currentText())
+                except:
+                    get_sample_rate = str(self.tableWidget_iq_playback.item(0,4).text())
                 get_data_type = str(self.tableWidget_iq_playback.cellWidget(0,5).currentText())   
                 get_repeat = str(self.tableWidget_iq_playback.cellWidget(0,6).currentText()) 
                 get_filepath = str(self.textEdit_iq_playback_filepath.toPlainText())
