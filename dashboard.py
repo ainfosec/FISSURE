875ba2475d8dfd07519fc732ab96b2a599322171
Chris Poore <poorec@ainfosec.com>
2023-07-17 15:32:51 -0400

    Automatic identification system (AIS) tools

--- dashboard.py
+++ dashboard.py
@@ -1447,6 +1447,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         self.textEdit_iq_start.textChanged.connect(self._slotIQ_StartChanged)
         self.textEdit_iq_end.textChanged.connect(self._slotIQ_EndChanged)
         self.plainTextEdit_pd_bit_viewer_hex.textChanged.connect(self._slotPD_BitViewerHexChanged)
+        self.plainTextEdit_pd_bit_viewer_bits.textChanged.connect(self._slotPD_BitViewerBitsChanged)
         self.textEdit_archive_extension.textChanged.connect(self._slotArchiveDownloadRefreshClicked)
 
         # Tree Widgets
@@ -1521,7 +1522,6 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         self.action4G_IMSI_Catcher.triggered.connect(self._slotMenu4G_IMSI_CatcherClicked)
         self.actionInspectrum.triggered.connect(self._slotMenuInspectrumClicked)
         self.actionOpenCPN.triggered.connect(self._slotMenuOpenCPN_Clicked)
-        self.actionAis_rx.triggered.connect(self._slotMenuAis_rxClicked)
         self.actionProtocol_Spreadsheet.triggered.connect(self._slotMenuProtocolSpreadsheetClicked)
         self.actionGrgsm_scanner.triggered.connect(self._slotMenuGrgsm_scannerClicked)
         self.actionKalibrate.triggered.connect(self._slotMenuKalibrateClicked)
@@ -1758,6 +1758,11 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         self.actionGHex.triggered.connect(self._slotMenuGHexClicked)
         self.actionqFlipper.triggered.connect(self._slotMenu_qFlipperClicked)
         self.actionGpick.triggered.connect(self._slotMenuGpickClicked)
+        self.actionAIVDM_AIVDO_Decoding.triggered.connect(self._slotMenuAIVDM_AIVDO_DecodingClicked)
+        self.actionAIS_VDM_VDO_Decoder.triggered.connect(self._slotMenuAIS_VDM_VDO_DecoderClicked)
+        self.actionAIS_Online_Decoder.triggered.connect(self._slotMenuAIS_OnlineDecoderClicked)
+        self.actionpyais_GitHub.triggered.connect(self._slotMenu_pyaisGitHubClicked)
+        self.actionais_rx.triggered.connect(self._slotMenuStandalone_ais_rxClicked)
 
         # Tab Widgets
         self.tabWidget_tsi.currentChanged.connect(self._slotTSI_TabChanged)
@@ -18798,19 +18803,6 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         else:
             proc=subprocess.Popen('gnome-terminal -- ' + expect_script_filepath + ' "opencpn"', shell=True)
 
-    def _slotMenuAis_rxClicked(self):
-        """ Launches ais_rx.
-        """
-        # Issue the Command
-        expect_script_filepath = os.path.dirname(os.path.realpath(__file__)) + "/Tools/expect_script"
-        if self.operating_system == 'DragonOS FocalX' or self.operating_system == 'Kali':
-            ais_rx_command = 'python3 ' + os.path.dirname(os.path.realpath(__file__)) + "/Custom_Blocks/maint-3.10/gr-ais/apps/ais_rx.py"
-            command_text = 'qterminal -e ' + expect_script_filepath + ' "' + ais_rx_command + '"'
-        else:
-            ais_rx_command = 'python3 ' + os.path.dirname(os.path.realpath(__file__)) + "/Custom_Blocks/maint-3.10/gr-ais/apps/ais_rx.py"
-            command_text = 'gnome-terminal -- ' + expect_script_filepath + ' "' + ais_rx_command + '"'
-        proc = subprocess.Popen(command_text, shell=True)
-
     def _slotMenuProtocolSpreadsheetClicked(self):
         """ Opens the Protocols_and_More.xlsx spreadsheet.
         """
@@ -20740,6 +20732,9 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         # Display the ASCII
         get_hex_str = str(self.plainTextEdit_pd_bit_viewer_hex.toPlainText()).replace(" ","")
 
+        # Update Counter
+        self.label2_pd_bit_viewer_nibbles.setText(str(len(get_hex_str.replace('\t','').replace('\n',''))))
+        
         # Split by Line
         get_hex = get_hex_str.splitlines()
         new_ascii = ""
@@ -20748,7 +20743,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
                 new_ascii = new_ascii + str(bytes.fromhex(get_hex[n]))[2:-1] + "\n"
 
         self.plainTextEdit_pd_bit_viewer_ascii.setPlainText(new_ascii)
-
+        
     def _slotMenuProtocolCSV_Clicked(self):
         """ Opens a CSV with RF protocols sorted by frequency.
         """
@@ -23529,7 +23524,7 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
         # Open the Flow Graph in GNU Radio Companion
         filepath = os.path.dirname(os.path.realpath(__file__)) + "/Flow\ Graph\ Library/Standalone\ Flow\ Graphs/pocsagtx.grc"
         osCommandString = "gnuradio-companion " + filepath
-        os.system(osCommandString+ " &")
+        os.system(osCommandString + " &")
 
     def _slotMenu_dump978_Clicked(self):
         """ Opens a terminal with an example dump978 command.
@@ -25889,7 +25884,47 @@ class MainWindow(QtWidgets.QMainWindow, form_class):
             proc=subprocess.Popen('qterminal -e ' + expect_script_filepath + ' "gpick"', shell=True)
         else:
             proc=subprocess.Popen('gnome-terminal -- ' + expect_script_filepath + ' "gpick"', shell=True)
-
+            
+    def _slotMenuAIVDM_AIVDO_DecodingClicked(self):
+        """ Opens AIVDM AIVDO Decoding in a browser.
+        """
+        # Open a Browser
+        os.system("sensible-browser https://gpsd.gitlab.io/gpsd/AIVDM.html &")
+        
+    def _slotMenuAIS_VDM_VDO_DecoderClicked(self):
+        """ Opens AIS VDM VDO Decoder in a browser.
+        """
+        # Open a Browser
+        os.system("sensible-browser https://www.maritec.co.za/aisvdmvdodecoding &")
+        
+    def _slotMenuAIS_OnlineDecoderClicked(self):
+        """ Opens AIS Online Decoder in a browser.
+        """
+        # Open a Browser
+        os.system("sensible-browser https://www.aggsoft.com/ais-decoder.htm &")
+        
+    def _slotMenu_pyaisGitHubClicked(self):
+        """ Opens pyais GitHub in a browser.
+        """
+        # Open a Browser
+        os.system("sensible-browser https://github.com/M0r13n/pyais &")
+        
+    def _slotPD_BitViewerBitsChanged(self):
+        """ Updates the bit counter in the Data Viewer.
+        """
+        # Obtain the Count
+        get_bit_count = len(str(self.plainTextEdit_pd_bit_viewer_bits.toPlainText()).replace(' ','').replace('\t','').replace('\n',''))
+        
+        # Update the Bits
+        self.label2_pd_bit_viewer_bits.setText(str(get_bit_count))
+        
+    def _slotMenuStandalone_ais_rxClicked(self):
+        """ Opens the standalone flow graph in GNU Radio Companion.
+        """
+        # Open the Flow Graph in GNU Radio Companion
+        filepath = os.path.dirname(os.path.realpath(__file__)) + "/Flow\ Graph\ Library/Standalone\ Flow\ Graphs/ais_rx.grc"
+        osCommandString = "gnuradio-companion " + filepath
+        os.system(osCommandString + " &")
 
 
 class HelpMenuDialog(QtWidgets.QDialog, form_class6):
